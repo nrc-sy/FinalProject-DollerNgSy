4dd6b87783cad5cb20f03c1bf76f60e2aa270c41
gideng <gide.alden.ng@student.ateneo.edu>
2026-04-18 11:44:08 +0800

    changed settings of settings.py of Shemu

--- FinalProject-DollerNgSy/Shemu/Shemu/settings.py
+++ FinalProject-DollerNgSy/Shemu/Shemu/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+pc8-^&2!%x@i5sr5w2((o4h-pf#dn)dov1z6(%6+378$2vmch
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'payroll_app'
 ]
 
 MIDDLEWARE = [
@@ -104,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Manila'
 
 USE_I18N = True
 
@@ -115,3 +116,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+LOGIN_URL = '/login/'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/login/'
