6b583014fc15915419323b6e59f522fc4201337a
rsy <nathan.riley.sy@student.ateneo.edu>
2026-04-19 20:46:33 +0800

    added login required n fixed employee_list in update employee in views

--- FinalProject-DollerNgSy/Shemu/payroll_app/views.py
+++ FinalProject-DollerNgSy/Shemu/payroll_app/views.py
@@ -80,13 +80,14 @@ def create_employee(request):
     return render(request, 'payroll_app/create_employee.html')
 
 # Update Employee
+@login_required
 def update_employee(request, pk):
     employee = get_object_or_404(Employee, pk=pk)
     if request.method == "POST":
         form = EmployeeForm(request.POST, instance=employee)
         if form.is_valid():
             form.save()
-            return redirect('employees_list')
+            return redirect('employee_list')
     else:
         form = EmployeeForm(instance=employee)
     return render(request, 'payroll_app/update_employee.html', {'form': form, 'employee': employee})
